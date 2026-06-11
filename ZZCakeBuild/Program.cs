2c408fcf00a470ddb2652984426a3ce0f79ca4c6
Paige Marincak <pmarincak@hotmail.com>
2026-06-11 18:17:21 -0400

    Fixed CakeBuild

--- ZZCakeBuild/Program.cs
+++ ZZCakeBuild/Program.cs
@@ -26,7 +26,7 @@ namespace CakeBuild
 
     public class BuildContext : FrostingContext
     {
-        public const string ProjectName = "Lunchbox";
+        public const string ProjectName = "PlayerCorpse";
         public string BuildConfiguration { get; }
         public string Version { get; }
         public string Name { get; }
